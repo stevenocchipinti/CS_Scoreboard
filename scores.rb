37876c20d7ab875dd1c0b718b8f64ea57cb8b4da
Steven Occhipinti <dev@stevenocchipinti.com>
2011-07-25 00:10:59 +1000

    Make /scores output a simple string

--- scores.rb
+++ scores.rb
@@ -1,9 +1,33 @@
+# CS-ScoreBoard
+
+# To run: ruby -rubygems scores.rb 
+
 require 'sinatra'
+require 'sqlite3'
 
 get '/' do
-  'Put this in your pipe and smoke it!'
+  redirect to('/scores')
 end
 
-get '/view/:stuff' do
-  haml :view
+get '/scores' do
+  # This query will fetch ALL statistics from the DB
+  sql = "select * from kills;"
+
+  db = SQLite3::Database.new("cs.db")
+  db.results_as_hash = true
+
+  result = ""
+  db.execute(sql) do |row|
+    result << "#{row['killer']} killed #{row['killed']} with a #{row['weapon']}"
+    result << row['headshot'] == 1 ? " IN THE HEAD!" : ""
+  end
+
+  return result
+
 end
+
+#get '/view/:stuff' do
+  #haml :view
+#end
+
+
