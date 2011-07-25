486430b42d66eee1e5fccd9010f270fb692dbd6d
Steven Occhipinti <dev@stevenocchipinti.com>
2011-07-25 17:22:31 +1000

    Watch for UDP traffic instead of a log file!
    The server can be configured to send log info to a UDP server, and this
    works much better than trying to monitor old log files that arent
    updated instantaneously.

--- daemon.rb
+++ daemon.rb
@@ -1,37 +1,44 @@
 require 'daemon_spawn'
 require 'sqlite3'
+require 'socket'
 
 class MyServer < DaemonSpawn::Base
 
   def start(args)
     # Process command-line args to get the logfile
-    abort "USAGE: daemon.rb LOGFILE" if args.empty?
-    @filename = args.first
+    #abort "USAGE: daemon.rb LOGFILE" if args.empty?
+    #@filename = args.first
 
-    file = File.open(@filename)
-    puts "Log parsing daemon (#{self.index}) started @ #{Time.new}"
+    # Configuration
+    port = 1234
+
+    # Connect to the DB to request a hash of results later
+    db = SQLite3::Database.new("cs.db")
+    db.results_as_hash = true
+
+    # Start listening for UDP data
+    sock = UDPSocket.new
+    sock.bind("0.0.0.0", port)
+
+    time = Time.new.strftime "%d/%m/%Y@%H:%M:%S"
+    puts "#{time}: Log parsing daemon listening on UDP #{port}"
 
     # Loop forever!
     loop do
-      # Connect to the DB to get a hash of results
-      db = SQLite3::Database.new("cs.db")
-      db.results_as_hash = true
-
-      # Process the new lines
-      while (line = file.gets)
-        # Generate an appropriate SQL insert statement
-        sql = log_line_to_sql_insert(line)
-        # Debug
-        puts "Parsing {{ #{line} }}".gsub(/[\n\r]/,"")
-        puts "SQL     {{ #{sql} }}"
-        # Write to the database
-        db.execute(sql)
-      end
-      sleep 10
+      # Read 1024 bytes of UDP data from the socket
+      data, addr = sock.recvfrom(1024)
+      # Generate an appropriate SQL insert statement
+      puts "Parsing {{ #{data} }}".gsub(/[\n\r]/,"")
+      sql = log_line_to_sql_insert(data)
+      puts "SQL     {{ #{sql} }}"
+      # Write to the database
+      db.execute(sql)
     end
   end
 
   def stop
+    time = Time.new.strftime "%d/%m/%Y@%H:%M:%S"
+    puts "#{time}: Log parsing daemon shutting down"
   end
 
   def log_line_to_sql_insert(line)
