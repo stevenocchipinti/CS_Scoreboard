b7ff8fab84fa1803b5c310c045f2b26fe615d1a4
Steven Occhipinti <dev@stevenocchipinti.com>
2011-07-26 21:56:51 +1000

    Add an interface for daemon control
    When browsing to the root url, the daemon will be started and you will
    be redirected to the scoreboard

--- scores.rb
+++ scores.rb
@@ -1,14 +1,16 @@
 # CS-ScoreBoard
 
-# To run: ruby -rubygems scores.rb 
+# To run: ruby -rubygems scores.rb
 
 require 'sinatra'
 require 'sqlite3'
 
 get '/' do
-  redirect to('/scores')
+  redirect to('/daemon/init')
 end
 
+
+# The scoreboard
 get '/scores' do
   # This query will fetch ALL statistics from the DB
   sql = "SELECT killer,
@@ -22,7 +24,24 @@ get '/scores' do
   db.results_as_hash = true
 
   @stats = db.execute(sql)
-
   haml :scores
+end
 
+
+# The user can control the daemon from here
+get '/daemon' do
+  @result = `ruby daemon.rb status`
+  haml :daemon
+end
+
+# When the app starts up '/' should redirect to this
+get '/daemon/init' do
+  @result = `ruby daemon.rb start`
+  redirect to('/scores')
+end
+
+# This handles executing the daemons actions
+get '/daemon/:action' do
+  @result = `ruby daemon.rb #{params[:action]}`
+  redirect to('/daemon')
 end
