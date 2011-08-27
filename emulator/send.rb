8e8fb7c368d206823db5ca424c8d17a9a1f403c9
Steven Occhipinti <dev@stevenocchipinti.com>
2011-08-27 16:43:48 +1000

    Add an emulator to send sample UDP data
    This replays an old log file from CS:S

---
+++ emulator/send.rb
@@ -0,0 +1,22 @@
+require 'socket'
+
+# Configuration
+udphost = "127.0.0.1"
+udpport = 1111
+udpflags = 0
+file = "test.log"
+
+# Open the UDP socket so we can write to it
+sock = UDPSocket.new
+
+# Read the given file and send the contents via UDP
+begin
+    file = File.new(file, "r")
+    while (line = file.gets)
+      sock.send(line, udpflags, udphost, udpport)
+    end
+    file.close
+rescue => err
+    puts "Error: #{err}"
+    err
+end
